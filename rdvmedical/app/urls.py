8bbd005780d87b8f54bc933454d515d17a783293
amiba <Vous@exemple.com>
2024-04-25 02:33:12 +0200

    update

--- rdvmedical/app/urls.py
+++ rdvmedical/app/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from .views import index
 
 urlpatterns = [
-    path("siterdv/", include("siterdv.urls")),
     path("admin/", admin.site.urls),
+    path("", index),
 ]
